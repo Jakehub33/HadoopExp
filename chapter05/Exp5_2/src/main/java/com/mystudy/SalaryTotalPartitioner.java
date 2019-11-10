f3b2e09ce6f0ac0d96a7e2f4307a5f81eb0afc1d
Jakehub33 <467323608@qq.com>
2019-11-10 20:56:13 +0800

    实验五5.2代码

---
+++ chapter05/Exp5_2/src/main/java/com/mystudy/SalaryTotalPartitioner.java
@@ -0,0 +1,23 @@
+package com.mystudy;
+
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mapreduce.Partitioner;
+//                                    map-outputs:k2,v2-->NullWritable, Employee
+public class SalaryTotalPartitioner extends Partitioner<NullWritable, Employee>{
+
+    @Override
+    public int getPartition(NullWritable k2, Employee v2, int numPatition) {
+
+        //如何分区: 每个部门放在一个分区
+        if(v2.getSal() < 1500) {
+            //放入1号分区中
+            return 1%numPatition;// 1%3=1
+        }else if(v2.getSal() >=1500 && v2.getSal() < 3000){
+            //放入2号分区中
+            return 2%numPatition;// 2%3=2
+        }else {
+            //放入3号分区中
+            return 3%numPatition;// 3%3=0
+        }
+    }
+}
